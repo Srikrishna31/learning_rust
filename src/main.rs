519f64389a30e17b96809fd892dceb243ef90171
Krishna Addepalli <coolkrishna31@gmail.com>
2022-10-20 11:42:33 +0200

    Added a simple gcd function to main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
+
+    println!("Gcd of 5 and 7 is {}", gcd(5, 7));
+}
+
+fn gcd(mut n:u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m=n;
+            n=t;
+        }
+        m=m%n;
+    }
+    n
 }
