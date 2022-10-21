f1108f326ab32026c022243ff914c00bcf780570
Krishna Addepalli <krishna.addepalli@kpit.com>
2022-10-21 08:54:53 +0200

    WIP: Written out serial mandelbrot set generator

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -19,9 +19,35 @@ All Rust functions are thread-safe.
  */
 
 use num::Complex;
+use std::env;
 
 fn main() {
-    println!("Hello, world!");
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT", args[0]);
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x')
+        .expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3])
+        .expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4])
+        .expect("error parsing lower right corner point");
+
+    //A macro call vec![v;n] creates a vector n elements long whose elements are initialized to v,
+    // so the following code creates a vector of zeros whose length is bounds.0*bounds.1, where
+    //bounds is the image resolution parsed from the command line.
+    let mut pixels = vec![0;bounds.0*bounds.1];
+
+    // The expression &mut pixels borrows a mutable reference to out pixel buffer, allowing render
+    // to fill it with computed grayscale values, even while pixels remains the vector's owner.
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds)
+        .expect("error writing PNG file");
 }
 
 //Below is a documentation comment, which the rustdoc can parse and produce online documentation.
@@ -62,12 +88,25 @@ fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
             match(T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
                 (Ok(l), Ok(r)) => Some((l,r)),
                 //The wildcard pattern _ matches anything and ignores its value.
-                _ => None
+                (Err(e), _) => {
+                    None
+                }
+                (_, Err(e)) => {
+                    None
+                }
             }
         }
     }
 }
 
+
+/// Parse a pair of floating-point numbers separated by a comma as a complex number
+fn parse_complex(s:&str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex{re, im}),
+        None => None
+    }
+}
 
 #[test]
 fn test_parse_pair() {
@@ -75,7 +114,102 @@ fn test_parse_pair() {
     assert_eq!(parse_pair::<i32>("10,", ','),None);
     assert_eq!(parse_pair::<i32>(",10", ','),None);
     assert_eq!(parse_pair::<i32>("10,20", ','),Some((10,20)));
+    assert_eq!(parse_pair::<i32>("10x20", 'x'), Some((10, 20)));
     assert_eq!(parse_pair::<i32>("10,20xy", ','),None);
     assert_eq!(parse_pair::<f64>("0.5x", 'x'),None);
-    assert_eq!(parse_pair::<f64>("0.5x1.5,", 'x'),Some((0.5, 1.5)));
+    assert_eq!(parse_pair("10.0x20.0", 'x'),Some((10.0, 20.0)));
+    assert_eq!(parse_pair("0.5x1.5", 'x'),Some((0.5, 1.5)));
+}
+
+
+#[test]
+fn test_parse_complex(){
+    assert_eq!(parse_complex("1.25,-0.0625"),
+                Some(Complex{re:1.25, im:-0.0625}));
+
+    assert_eq!(parse_complex(", -0.0625"), None);
+}
+
+
+/// Given the row and column of a pixel in the output image, return the corresponding point on the
+/// complex plane.
+///
+/// `bounds` is a pair giving the width and height of the image in pixels.
+/// `pixel` is a (column,row) pair indicating a particular pixel in that image.
+/// The `upper_left` and lower_left` parameters are points on the complex plane designating the
+/// area our image covers.
+fn pixel_to_point(bounds:(usize, usize),
+                  pixel:(usize, usize),
+                  upper_left: Complex<f64>,
+                  lower_right: Complex<f64>)
+-> Complex<f64>
+{
+    let (width, height) = (lower_right.re - upper_left.re,
+                                upper_left.im - lower_right.im);
+    //<> as f64 is Rust syntax for type conversion; Rust generally refuses to convert between
+    //numeric types implicitly; you must write out the conversions you need.
+    Complex{re: upper_left.re+pixel.0 as f64 * width / bounds.0 as f64,
+            im:upper_left.im -pixel.1 as f64 * height / bounds.1 as f64}
+            //Why subtraction here? pixel.1 increases as we go down, but the imaginary
+            //component increases as we go up.
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(pixel_to_point((100,200), (25,175),
+                              Complex{re:-1.0, im:1.0},
+                              Complex{re:1.0, im:-1.0}),
+               Complex{re:-0.5, im:-0.75})
+}
+
+
+/// Render a rectangle of the Mandelbrot set into a buffer of pixels.
+///
+/// The `bounds` argument gives the width and height of the buffer `pixels`,
+/// which holds one grayscale pixel per byte. The `upper_left` and `lower_right` arguments specify
+/// points on the complex plane corresponding to the upper-left and lower-right corners of the
+/// pixel buffer.
+fn render(pixels: &mut[u8],
+          bounds: (usize, usize),
+          upper_left:Complex<f64>,
+          lower_right:Complex<f64>)
+{
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0..bounds.1 {
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column, row),
+                                                    upper_left, lower_right);
+            pixels[row*bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8
+            };
+        }
+    }
+}
+
+use image::ColorType;
+use image::png::PNGEncoder;
+use std::fs::File;
+
+/// Write the buffer `pixels`, whose dimensions are given by `bounds`, to the file named `filename`
+fn write_image(filename: &str, pixels : &[u8], bounds: (usize, usize)) -> Result<(), std::io::Error>
+{
+    // To properly handle File::create's result, we need to match on its return value, as below:
+    // let output = match File::create(filename) {
+    //     Ok(f) => f,
+    //     Err(e) => {
+    //          return Err(e);
+    //  }
+    //};
+    //This kind of match statement is such a common pattern in Rust that the language provides the ?
+    // operator as shorthand for the whole thing.
+    let output = File::create(filename)?;
+
+    let encoder= PNGEncoder::new(output);
+    encoder.encode(pixels,
+                    bounds.0 as u32, bounds.1 as u32,
+                    ColorType::Gray(8))?;
+
+    Ok(())
 }
