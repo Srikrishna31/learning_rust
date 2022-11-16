b7b35d06082118b6f59259e1f1b9818e334e7ae9
Krishna Addepalli <coolkrishna31@gmail.com>
2022-11-16 12:02:32 +0100

    Macros: Finished with the chapter

--- macros/src/json.rs
+++ macros/src/json.rs
@@ -41,3 +41,105 @@ macro_rules! my_vec {
         vec![$($x),*]
     };
 }
+
+
+#[derive(Clone, PartialEq, Debug)]
+pub enum Json {
+    Null,
+    Boolean(bool),
+    Number(f64),
+    String(String),
+    Array(Vec<Json>),
+    Object(Box<HashMap<String, Json>>)
+}
+
+impl From<bool> for Json {
+    fn from(b: bool) -> Json {
+        Json::Boolean(b)
+    }
+}
+
+impl From<String> for Json {
+    fn from(s: String) -> Json {
+        Json::String(s)
+    }
+}
+
+impl<'a> From<&'a str> for Json {
+    fn from(s: &'a str) -> Json {
+        Json::String(s.to_string())
+    }
+}
+
+/// For all numeric types, its better to write a macro and invoke it.
+macro_rules! impl_from_num_for_json {
+    ( $($t:ident)* ) => {
+        $(
+            impl From<$t> for Json {
+                fn from(n: $t) -> Json {
+                    Json::Number(n as f64)
+                }
+            }
+        )*
+    };
+}
+
+impl_from_num_for_json!(u8 i8 u16 i16 u32 i32 u64 i64 u128 i128 usize isize f32 f64);
+
+pub use std::collections::HashMap;
+pub use std::boxed::Box;
+pub use std::string::ToString;
+
+/// Token trees: it is either a properly matched pair of brackets, (...), [...], or {...}, and
+/// everything in between, including nested token trees, or a single token that isn't a bracket, like
+/// 1926 or "Knots".
+/// Token trees are exactly what we need for our json! macro. Every JSON value is a single token tree:
+/// numbers, strings, Boolean values, and null are all single tokens; objects and arrays are bracketed.
+#[macro_export]
+macro_rules! json {
+    (null) => {
+        // $crate acts like an absolute path to the root module of the crate where the macro was
+        // defined. Instead of saying Json, we can write $crate::Json, which works even if Json was
+        // not imported.
+        $crate::Json::Null
+    };
+    ([$($element:tt),*]) => {
+        $crate::Json::Array(vec![$(json!($element)),*])
+    };
+    ({ $($key:tt : $value:tt),*}) => {
+        $crate::Json::Object($crate::json::Box::new(vec![
+            $( ($crate::json::ToString::to_string($key), json!($value)) ),*
+        ].into_iter().collect()))
+    };
+    ($other:tt) => {
+        $crate::Json::from($other)  //Handle Boolean/Number/String
+    }
+}
+
+/// Whenever macros use temporary variables in the code expansion, Rust renames those variables, after
+/// pasting it in the target place. This feature, first implemented in Scheme macros, is called
+/// hygiene, and so Rust is said to have hygienic macros.
+/// This prevents name collisions with the local variables of the code calling the macros.
+#[test]
+fn json_null() {
+    assert_eq!(json!(null), Json::Null);
+}
+
+
+#[test]
+fn json_array_with_json_element() {
+    let macro_generate_value = json!(
+        [
+            //valid json that doesn't match `$element:expr`
+            {
+                "pitch":440.0
+            }
+        ]
+    );
+
+    let hand_coded_value = Json::Array(vec![
+        Json::Object(Box::new(vec![("pitch".to_string(), Json::Number(440.0))].into_iter().collect()))
+    ]);
+
+    assert_eq!(macro_generate_value, hand_coded_value);
+}
--- macros/src/main.rs
+++ macros/src/main.rs
@@ -1,4 +1,10 @@
+#![recursion_limit="256"]
+
+/// The compiler imposes a recursion limit on macros: 64 calls, by default. You can adjust it by
+/// adding the below attribute at the top of the crate where the macro is used.
 #[macro_use] mod json;
+use json::Json;
+
 
 fn main() {
     println!("Hello, world!");
@@ -6,4 +12,32 @@ fn main() {
     let v = my_vec![1,2,3];
 
     println!("{:?}", v);
+
+    let students_macro = json!([
+        {
+            "name" : "Jim Blandy",
+            "class_of": 1926,
+            "major": "Tibetan throat singing"
+        },
+        {
+            "name": "Jason Orendorff",
+            "class_of": 1702,
+            "major": "Knots"
+        }
+    ]);
+
+    let students_hand_coded = Json::Array(vec![
+        Json::Object(Box::new(vec![
+            ("name".to_string(), Json::String("Jim Blandy".to_string())),
+            ("class_of".to_string(), Json::Number(1926.0)),
+            ("major".to_string(), Json::String("Tibetan throat singing".to_string()))
+        ].into_iter().collect())),
+        Json::Object(Box::new(vec![
+            ("name".to_string(), Json::String("Jason Orendorff".to_string())),
+            ("class_of".to_string(), Json::Number(1702.0)),
+            ("major".to_string(), Json::String("Knots".to_string()))
+        ].into_iter().collect()))
+    ]);
+
+    assert_eq!(students_macro, students_hand_coded);
 }
